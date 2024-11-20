15892db33f63c61b10f35b0f0a1563c57d3bb60f
JB OLIVIER <jb.olivier57@outlook.fr>
2024-11-20 09:25:16 +0000

    correction delete vehocle

--- src/controller/delete.ts
+++ src/controller/delete.ts
@@ -1,6 +1,5 @@
 import { VehicleStore } from '../store/vehicle';
 import { Request, Response } from 'express';
-
 interface Parameters {
   id: string;
 }
@@ -9,7 +8,9 @@ export class DeleteVehicleController {
   constructor(private readonly vehicleStore: VehicleStore) {}
 
   public async handle(req: Request<Parameters>, res: Response): Promise<void> {
-    res.status(500).send();
+    const deleterequest  = { id : parseInt(req.params.id), };
+    await this.vehicleStore.deleteVehicle(deleterequest);
+    res.status(204).send();
   }
 }
 
